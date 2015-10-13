a65a8ceca3a11f542884807fbbb72e1270a6c216
王鹏飞 <wangpfitta@163.com>
2015-10-13 12:00:04 +0800

    目录结构化

--- src/main/webapp/static/script/moudle/head/index.js
+++
@@ -1,9 +0,0 @@
-var app=angular.module('welcome', []);
-
-app.controller('company', function($scope, $http){
-    $http.get('indexRest.do').
-        success(function(data) {
-            console.log(data);
-            $scope.company = data
-    });
-});
